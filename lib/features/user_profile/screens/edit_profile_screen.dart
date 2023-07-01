cfb73d1b1277c6ecfccc012416b5a9d948355bdc
Nidhinlal <lalnidhinp02@gmai.com>
2023-07-01 17:03:17 +0530

    full completed

--- lib/features/user_profile/screens/edit_profile_screen.dart
+++ lib/features/user_profile/screens/edit_profile_screen.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:redite_clone/features/auth/controller/auth_controller.dart';
+import 'package:redite_clone/resposive/resposive.dart';
 
 import '../../../core/common/error_text.dart';
 import '../../../core/common/loader.dart';
@@ -110,96 +111,101 @@ class _EditeProfileScreenState extends ConsumerState<EditeProfileScreen> {
             ),
             body: isLoading
                 ? const Loader()
-                : Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: 200,
-                          child: Stack(
-                            children: [
-                              GestureDetector(
-                                onTap: selectBannerImage,
-                                child: DottedBorder(
-                                  borderType: BorderType.RRect,
-                                  radius: const Radius.circular(10),
-                                  dashPattern: const [10, 4],
-                                  strokeCap: StrokeCap.round,
-                                  color:
-                                      currentTheme.textTheme.bodyText2!.color!,
-                                  child: Container(
-                                    width: double.infinity,
-                                    height: 150,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    child: bannerWebFile != null
-                                        ? Image.memory(
-                                            bannerWebFile!,
-                                            fit: BoxFit.cover,
-                                          )
-                                        : bannerFile != null
-                                            ? Image.file(
-                                                bannerFile!,
-                                                fit: BoxFit.cover,
-                                              )
-                                            : user.banner.isEmpty ||
-                                                    user.banner ==
-                                                        Constants.bannerDefault
-                                                ? const Center(
-                                                    child: Icon(
-                                                      Icons.camera_alt_outlined,
-                                                      size: 40,
+                : Responsive(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 200,
+                            child: Stack(
+                              children: [
+                                GestureDetector(
+                                  onTap: selectBannerImage,
+                                  child: DottedBorder(
+                                    borderType: BorderType.RRect,
+                                    radius: const Radius.circular(10),
+                                    dashPattern: const [10, 4],
+                                    strokeCap: StrokeCap.round,
+                                    color: currentTheme
+                                        .textTheme.bodyText2!.color!,
+                                    child: Container(
+                                      width: double.infinity,
+                                      height: 150,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: bannerWebFile != null
+                                          ? Image.memory(
+                                              bannerWebFile!,
+                                              fit: BoxFit.cover,
+                                            )
+                                          : bannerFile != null
+                                              ? Image.file(
+                                                  bannerFile!,
+                                                  fit: BoxFit.cover,
+                                                )
+                                              : user.banner.isEmpty ||
+                                                      user.banner ==
+                                                          Constants
+                                                              .bannerDefault
+                                                  ? const Center(
+                                                      child: Icon(
+                                                        Icons
+                                                            .camera_alt_outlined,
+                                                        size: 40,
+                                                      ),
+                                                    )
+                                                  : Image.network(
+                                                      user.banner,
+                                                      fit: BoxFit.cover,
                                                     ),
-                                                  )
-                                                : Image.network(
-                                                    user.banner,
-                                                    fit: BoxFit.cover,
-                                                  ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Positioned(
-                                bottom: 20,
-                                left: 20,
-                                child: GestureDetector(
-                                  onTap: selectProfileImage,
-                                  child: profileWebFile != null
-                                      ? CircleAvatar(
-                                          backgroundImage:
-                                              MemoryImage(profileWebFile!),
-                                          radius: 32,
-                                        )
-                                      : profileFile != null
-                                          ? CircleAvatar(
-                                              backgroundImage:
-                                                  FileImage(profileFile!),
-                                              radius: 32,
-                                            )
-                                          : CircleAvatar(
-                                              backgroundImage:
-                                                  NetworkImage(user.profilePic),
-                                              radius: 32,
-                                            ),
+                                Positioned(
+                                  bottom: 20,
+                                  left: 20,
+                                  child: GestureDetector(
+                                    onTap: selectProfileImage,
+                                    child: profileWebFile != null
+                                        ? CircleAvatar(
+                                            backgroundImage:
+                                                MemoryImage(profileWebFile!),
+                                            radius: 32,
+                                          )
+                                        : profileFile != null
+                                            ? CircleAvatar(
+                                                backgroundImage:
+                                                    FileImage(profileFile!),
+                                                radius: 32,
+                                              )
+                                            : CircleAvatar(
+                                                backgroundImage: NetworkImage(
+                                                    user.profilePic),
+                                                radius: 32,
+                                              ),
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        TextField(
-                          controller: nameController,
-                          decoration: InputDecoration(
-                            filled: true,
-                            hintText: 'Name',
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.blue),
-                              borderRadius: BorderRadius.circular(10),
+                              ],
                             ),
-                            border: InputBorder.none,
-                            contentPadding: const EdgeInsets.all(18),
                           ),
-                        )
-                      ],
+                          TextField(
+                            controller: nameController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              hintText: 'Name',
+                              focusedBorder: OutlineInputBorder(
+                                borderSide:
+                                    const BorderSide(color: Colors.blue),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              border: InputBorder.none,
+                              contentPadding: const EdgeInsets.all(18),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
           ),
